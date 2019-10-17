047f312fd6d693a5be9b527bb5a9852d3d464eb1
sksalve <sachinsalve90@outlook.com>
2019-10-17 17:16:14 +0530

    monk working

--- auth/index.js
+++ auth/index.js
@@ -2,48 +2,70 @@ const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
 const db = require("../db/connection");
+const bcrypt = require("bcryptjs");
 
 const users = db.get("users");
 
 // users.index("username");
 users.createIndex("username", {
-    unique: true
+  unique: true
 });
 
 const schema = Joi.object().keys({
-    username: Joi.string()
-        .regex(/(^[a-zA-Z0-9_]*$)/)
-        .min(3)
-        .max(30)
-        .required(),
-    password: Joi.string()
-        .min(6)
-        .required()
+  username: Joi.string()
+    .regex(/(^[a-zA-Z0-9_]*$)/)
+    .min(3)
+    .max(30)
+    .required(),
+  password: Joi.string()
+    .min(6)
+    .required()
 });
 
 router.get("/", (req, res) => {
-    res.json({
-        message: "🔑 🔒"
-    });
+  res.json({
+    message: "🔑 🔒"
+  });
 });
 
 //post route /auth/signup
 router.post("/signup", (req, res, next) => {
-    console.log("BODY", req.body);
-    const result = Joi.validate(req.body, schema);
-    if (result.error === null) {
-        users.findOne({
-            username: req.body.username
-        }).then(user => {
-            res.json({
-                user
-            });
+  console.log("BODY", req.body);
+  const result = Joi.validate(req.body, schema);
+  if (result.error === null) {
+    users
+      .findOne({
+        username: req.body.username
+      })
+      .then(user => {
+        res.json({
+          user
         });
-    } else {
-        next(result.error);
-    }
+        if (user) {
+          const error = new Error(
+            "User alreay exists, please choose another one!"
+          );
+        } else {
+          // let's hash password
+          hashedPassword = bcrypt.hash("bacon", 12, function(err, hash) {
+            if (err) {
+              res.json(err);
+            } else {
+              hash.json({ hashedPassword });
+            }
 
-    res.json(result);
+                const User  = users.insertOne({
+              username: req.body.username,
+              password: hashedPassword
+            });
+          });
+        }
+      });
+  } else {
+    next(result.error);
+  }
+
+//   res.json(User);
 });
 
 module.exports = router;
